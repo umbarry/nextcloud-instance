d1889dbea7e0fa8ecbd4c5e849987ceced7c9f3e
Umberto <uatania@outlook.com>
2024-12-10 15:45:24 +0100

    fix local config

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM php:8.1.2-apache
+
+# Installare il modulo PDO MySQL e altre dipendenze utili
+RUN apt-get update && apt-get install -y \
+    libpng-dev \
+    libjpeg-dev \
+    libfreetype6-dev \
+    libxml2-dev \
+    libzip-dev \
+    zip \
+    unzip \
+    && docker-php-ext-configure gd --with-freetype --with-jpeg \
+    && docker-php-ext-install gd \
+    && docker-php-ext-install mysqli pdo pdo_mysql \
+    && docker-php-ext-install pcntl zip \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Increase memory limit
+RUN echo "memory_limit = 512M" >> /usr/local/etc/php/conf.d/memory-limit.ini
+
+# Abilitare mod_rewrite per Apache (necessario per Nextcloud)
+RUN a2enmod rewrite
+
+# Impostare UID e GID (da sostituire con i tuoi valori)
+ARG UID=1000
+ARG GID=1000
+RUN groupmod -g ${GID} www-data && usermod -u ${UID} -g ${GID} www-data
+
+# Cambiare l'utente di default
+USER www-data
---
+++ app/config/config-prod.php
@@ -0,0 +1,43 @@
+<?php
+$CONFIG = array (
+    'instanceid' => getenv('NEXTCLOUD_INSTANCE_ID'),
+    'passwordsalt' => getenv('NEXTCLOUD_PASSWORD_SALT'),
+    'secret' => getenv('NEXTCLOUD_SECRET'),
+    'trusted_domains' => 
+    array (
+        0 => getenv('NEXTCLOUD_TRUSTED_DOMAIN'),
+    ),
+    'datadirectory' => getenv('NEXTCLOUD_DATADIRECTORY'),
+    'dbtype' => 'mysql',
+    'version' => '29.0.5.1',
+    'overwrite.cli.url' => 'https://' . getenv('NEXTCLOUD_TRUSTED_DOMAIN'),
+    'dbname' => getenv('MYSQL_DATABASE'),
+    'dbhost' => getenv('MYSQL_HOST'),
+    'dbport' => '',
+    'dbtableprefix' => 'oc_',
+    'mysql.utf8mb4' => true,
+    'dbuser' => getenv('MYSQL_USER'),
+    'dbpassword' => getenv('MYSQL_PASSWORD'),
+    'installed' => true,
+    'htaccess.RewriteBase' => '/',
+    'memories.db.triggers.fcu' => true,
+    'memories.exiftool' => '/var/www/nextcloud/apps/memories/bin-ext/exiftool-amd64-glibc',
+    'memories.vod.path' => '/var/www/nextcloud/apps/memories/bin-ext/go-vod-amd64',
+    'maintenance' => false,
+    'enable_previews' => true,
+    'enabledPreviewProviders' => 
+    array (
+        0 => 'OC\\Preview\\TXT',
+        1 => 'OC\\Preview\\Image',
+        2 => 'OC\\Preview\\Movie',
+        3 => 'OC\\Preview\\MP4',
+        4 => 'OC\\Preview\\GIF',
+    ),
+    'preview_max_x' => 300,
+    'preview_max_y' => 300,
+    'loglevel' => 2,
+    'maintenance_window_start' => 1,
+    'theme' => '',
+);
+
+
